2bcba2498971590b4b493fc631a2d9ac4d791a57
JagratiVerma1408 <vermajagrati212gmail.com>
2021-08-03 12:39:35 +0530

    Pushing Code

---
+++ app/src/main/java/com/example/covid_19tracker/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.covid_19tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(MainActivity2.this,MainActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+
+                }
+            }
+        };
+        // start thread
+        background.start();
+
+
+
+
+
+}
+}
